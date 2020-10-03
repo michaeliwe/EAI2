6bbfc4de098d36aecfa348f7e4c6dce88174c147
Michael Addryan Liwe <michaeliwe98@gmail.com>
2020-10-03 22:38:32 +0800

    update pegawai api

--- app/Http/Controllers/PegawaiAPIController.php
+++ app/Http/Controllers/PegawaiAPIController.php
@@ -10,10 +10,12 @@ class PegawaiAPIController extends Controller
     public function index()
     {
         $client = new Client;
+        do {
+            $response = $client->get("http://dummy.restapiexample.com/api/v1/employees");
+            $statusCode = $response->getStatusCode();
+        } while($statusCode != 200);
 
-        $response = $client->get("http://dummy.restapiexample.com/api/v1/employees");
         $contents = json_decode($response->getBody()->getContents(), true);
-
-        return view('pegawai.index', compact('contents'));
+        return response()->json($contents, 200);
     }
 }
